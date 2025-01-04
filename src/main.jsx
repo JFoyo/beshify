b96592d73c3dd95c7574bfafedf1346f5327c9b3
JFoyo <jaysonfoyo1999@gmail.com>
2025-01-04 19:25:51 +0800

    update packages

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import { ChakraProvider } from '@chakra-ui/react'
+import React from 'react';
+import ReactDOM from 'react-dom/client'; // Correct import for ReactDOM
+import App from './App.jsx';
+import { ChakraProvider } from '@chakra-ui/react';
+import { HashRouter as Router } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ChakraProvider>
-    <App />
-    </ChakraProvider>
+    <Router>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </Router>
   </React.StrictMode>,
-)
+);
