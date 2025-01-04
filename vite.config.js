22737ab2580798c92ff9b09acb971571dcc588e1
JFoyo <jaysonfoyo1999@gmail.com>
2025-01-04 19:05:19 +0800

    Update

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: "/beshify/",
   plugins: [react()],
+  server: {
+    open: true, // Automatically open the browser
+  },
 })
