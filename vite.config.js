35de0c10092a1f7384c116ce11dc2bdcefdc33d1
JFoyo <jaysonfoyo1999@gmail.com>
2025-01-04 19:11:26 +0800

    update package

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/beshify/",
   plugins: [react()],
-  server: {
-    open: true, // Automatically open the browser
-  },
-})
+  base: '/beshify/', // Replace 'your-repo-name' with the name of your GitHub repository
+});
